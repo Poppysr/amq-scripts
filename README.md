c5fa7ff2fe66c6fb1f2a32b18d9462be4b634e45
Poppysr <199649396+Poppysr@users.noreply.github.com>
2025-11-25 07:35:04 +0100

    Update with script and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# amq-scripts
+
+### [Avatar Dimmer](https://github.com/Poppysr/amq-scripts/blob/main/amqDimAllAvatars.user.js)
+---
+Left click a player's avatar while in a quiz to toggle dim on their avatar. Right click anywhere on the avatars' container to toggle dim on all the avatars in it. To adjust the dim, edit the value of dimValue. Made with 8 people in a lobby in mind, and likely breaks if used with more. The script was made to help keep track of players with guesses left in NGMC. It's my first script, feedback's welcome!
--- amqDimAllAvatars.user.js
+++ amqDimAllAvatars.user.js
@@ -5,14 +5,52 @@
 // @description  Dim avatars of players in a quiz by clicking
 // @author       Poppysr
 // @match        https://animemusicquiz.com/*
-// @downloadURL
-// @updateURL
+// @downloadURL  https://github.com/Poppysr/amq-scripts/blob/main/amqDimAllAvatars.user.js
+// @updateURL    https://github.com/Poppysr/amq-scripts/blob/main/amqDimAllAvatars.user.js
 // ==/UserScript==
 
+"use strict";
 if (typeof Listener === "undefined") return;
 const loadInterval = setInterval(() => {
-  if (document.getElementById("loadingScreen").classList.contains("hidden")) {
+  if (document.querySelector("#loadingScreen.hidden")) {
     clearInterval(loadInterval);
-    setup();
+    main();
   }
 }, 500);
+
+const dimValue = "0.4"; // 0 = invisible, 1 = opaque
+
+function main() {
+  $("#qpAvatarRowAvatarContainer").click((e) => {
+    if (e.target.closest(".qpAvatarInfoBarOuter")) return;
+    if (!e.target.closest(".qpAvatarContainer")) return;
+    const avatarContainer = e.target.closest(".qpAvatarContainerOuter");
+    if (avatarContainer) {
+      toggleDimOnClickedAvatar(avatarContainer);
+    }
+  });
+
+  $("#qpAvatarRowAvatarContainer").on("contextmenu", (e) => {
+    e.preventDefault();
+    const ifSomeDim = [...document.querySelectorAll(".qpAvatarContainerOuter")].some(avatar => parseFloat(window.getComputedStyle(avatar).opacity) < 1);
+    dimAllAvatars(ifSomeDim);
+  });
+
+  function dimAllAvatars(isDim) {
+    const isDimOpacity = isDim ? "1" : dimValue;
+    [...document.querySelectorAll(".qpAvatarContainerOuter")].forEach(elem => {
+      elem.style.opacity = isDimOpacity;
+    });
+  }
+
+  function toggleDimOnClickedAvatar(targetElem) {
+    const targetElemParent = targetElem.closest(".qpAvatarContainerOuter");
+    const currentOpacity = window.getComputedStyle(targetElemParent).opacity;
+
+    if (currentOpacity < 1) {
+      targetElemParent.style.opacity = "1";
+    } else if (currentOpacity == 1) {
+      targetElemParent.style.opacity = dimValue;
+    }
+  };
+}
